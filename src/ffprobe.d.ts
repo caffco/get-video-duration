de6807655edc0fc3f06141ceb176adbd89157170
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2025-11-26 17:37:02 +0100

    chore: Upgrade all dependencies and move from Jest, ESLint and Prettier to Vitest and Biome

--- src/ffprobe.d.ts
+++ src/ffprobe.d.ts
@@ -1,5 +1,5 @@
-declare module '@ffprobe-installer/ffprobe' {
-  export const path: string
-  export const version: string
-  export const url: string
+declare module "@ffprobe-installer/ffprobe" {
+	export const path: string;
+	export const version: string;
+	export const url: string;
 }
--- test/index.unit.test.ts
+++ test/index.unit.test.ts
@@ -1,34 +1,33 @@
-import 'jest'
+import { execa } from "execa";
+import { describe, expect, it, vi } from "vitest";
 
-import * as execa from 'execa'
+import getDuration from "../src";
 
-import getDuration from '../src'
+vi.mock("execa", () => ({
+	execa: vi.fn().mockResolvedValue({
+		stdout: 'duration="42.0"',
+	} as never),
+}));
 
-jest.mock('execa', () =>
-  jest.fn().mockResolvedValue({
-    stdout: 'duration="42.0"',
-  } as never)
-)
+const expectedVideoDurationThreshold = 0;
 
-const expectedVideoDurationThreshold = 0
+describe("get-video-duration", () => {
+	describe("when using a file path", () => {
+		it("Should use overriden ffprobe when provided", async () => {
+			const durationPromise = getDuration(
+				"fake file",
+				"the overriden path to ffprobe",
+			);
 
-describe('get-video-duration', () => {
-  describe('when using a file path', () => {
-    it('Should use overriden ffprobe when provided', async () => {
-      const durationPromise = getDuration(
-        'fake file',
-        'the overriden path to ffprobe'
-      )
+			expect(execa).toHaveBeenCalledWith(
+				"the overriden path to ffprobe",
+				expect.anything(),
+			);
 
-      expect(execa).toHaveBeenCalledWith(
-        'the overriden path to ffprobe',
-        expect.anything()
-      )
-
-      await expect(durationPromise).resolves.toBeCloseTo(
-        42.0,
-        expectedVideoDurationThreshold
-      )
-    })
-  })
-})
+			await expect(durationPromise).resolves.toBeCloseTo(
+				42.0,
+				expectedVideoDurationThreshold,
+			);
+		});
+	});
+});
